5739c8f2c6bc159addf758aefa8fd04b26f94a1e
Yanpeng Yu <yy594@login2.bouchet.ycrc.yale.edu>
2025-10-11 00:05:48 -0400

    run 2GPU benchmark

--- bench_serve.sh
+++ bench_serve.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+NUM_GPUS=$1
+EP_DEGREE=$2
+NUM_REPLICAS=$3
+BATCH_SIZE=$4
 RES_DIR=./results
 
 PORT=$(python3 -c 'import socket as s; sock=s.socket(); sock.bind(("",0)); print(sock.getsockname()[1]); sock.close()')
 
-vllm serve Qwen/Qwen3-30B-A3B --port $PORT >$RES_DIR/server.log 2>&1 &
+vllm serve Qwen/Qwen3-30B-A3B \
+    --port $PORT \
+    --tensor-parallel-size $EP_DEGREE \
+    --enable-expert-parallel \
+    --enable-eplb \
+    --eplb-config "{\"window_size\":1000,\"step_interval\":3000,\"num_redundant_experts\":$NUM_REPLICAS}" \
+    --max-num-batched-tokens $BATCH_SIZE \
+    --enable-chunked-prefill \
+    -O.level=3 \
+    >$RES_DIR/server.log 2>&1 &
 
 vllm bench serve \
     --model Qwen/Qwen3-30B-A3B \
@@ -17,3 +30,4 @@ vllm bench serve \
     --metric-percentiles 50,95,99 \
     --ready-check-timeout-sec 360 \
     --port $PORT
+    # --hf-output-len use this to increase decode ratio?
--- slurm_run_bench_serve.sh
+++ slurm_run_bench_serve.sh
@@ -1,4 +1,9 @@
 USR=pi_ak2579
 PART=gpu_h200
 
-srun -A $USR --mem=100g --gpus=h200:1 --partition=$PART --nodes=1 --cpus-per-task=8 --time=00:30:00 ./bench_serve.sh
+NUM_GPUS=$1
+EP_DEGREE=$2
+NUM_REPLICAS=$3
+BATCH_SIZE=$4
+
+srun -A $USR --mem=100g --gpus=h200:$NUM_GPUS --partition=$PART --nodes=1 --cpus-per-task=8 --time=00:30:00 ./bench_serve.sh $NUM_GPUS $EP_DEGREE $NUM_REPLICAS $BATCH_SIZE
