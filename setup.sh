d3416762f932c2793787f725de03ea72a381e9de
Yanpeng Yu <yy594@login2.bouchet.ycrc.yale.edu>
2025-10-11 00:07:15 -0400

    init

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd ~/project_pi_ak2579/yy594/vllm
-git checkout v0.11.0
+cd ~/vllm
+git checkout v0.11.0-gcp
 
 source .venv/bin/activate
 
-module load CUDA/12.8.0
-module load GCC/13.3.0
+# module load CUDA/12.8.0
+# module load GCC/13.3.0
 
 VLLM_USE_PRECOMPILED=1 uv pip install -U -e ".[bench]" --torch-backend=auto
 
-# python tools/generate_cmake_presets.py
+python tools/generate_cmake_presets.py
 cmake --preset release
 cmake --build --preset release --target install
