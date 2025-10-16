e26ded551b46d1982b4d7c2103ffc05e9e2d4973
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2025-10-16 17:06:53 -0400

    add prefill-only to measure prefill latency and chunked-prefill

--- bench_serve.sh
+++ bench_serve.sh
@@ -4,9 +4,11 @@ NUM_GPUS=$1
 EP_DEGREE=$2
 NUM_REPLICAS=$3
 BATCH_SIZE=$4
+PREFILL_ONLY=$5
+CHUNKED_PREFILL=$6
 RES_DIR=./results
 
-RUN_HASH=${NUM_GPUS}_${EP_DEGREE}_${NUM_REPLICAS}_${BATCH_SIZE}
+RUN_HASH=${NUM_GPUS}_${EP_DEGREE}_${NUM_REPLICAS}_${BATCH_SIZE}_${PREFILL_ONLY}_${CHUNKED_PREFILL}
 
 PORT=$(python3 -c 'import socket as s; sock=s.socket(); sock.bind(("",0)); print(sock.getsockname()[1]); sock.close()')
 
@@ -42,6 +44,10 @@ if (( NUM_REPLICAS > 0 )); then
   args+=( --eplb-config "{\"window_size\":100,\"step_interval\":10000000,\"num_redundant_experts\":${NUM_REPLICAS}}" )
 fi
 
+if (( CHUNKED_PREFILL > 0 )); then
+  args+=( --max-num-batched-tokens $BATCH_SIZE )
+fi
+
 unset VLLM_TORCH_PROFILER_DIR
 unset TOPK_DUMP_PREFIX
 vllm "${args[@]}" >"$RES_DIR/server_$RUN_HASH.log" 2>&1 &
@@ -60,17 +66,24 @@ cleanup() {
 }
 trap cleanup EXIT
 
-vllm bench serve \
-    --model Qwen/Qwen3-30B-A3B \
-    --dataset-name hf \
-    --dataset-path likaixin/InstructCoder \
-    --backend vllm \
-    --save-result \
-    --result-filename $RES_DIR/bench_result_$RUN_HASH.json \
-    --percentile-metrics ttft,tpot,itl,e2el \
-    --metric-percentiles 50,95,99 \
-    --ready-check-timeout-sec 240 \
-    --port $PORT \
-    --num-prompts 1024 \
+
+cli_args=(
+    --model Qwen/Qwen3-30B-A3B
+    --dataset-name hf
+    --dataset-path likaixin/InstructCoder
+    --backend vllm
+    --save-result
+    --result-filename "$RES_DIR"/bench_result_"$RUN_HASH".json
+    --percentile-metrics ttft,tpot,itl,e2el
+    --metric-percentiles 10,20,30,40,50,95,99
+    --ready-check-timeout-sec 240
+    --port "$PORT"
+    --num-prompts 1024
     --max-concurrency $((BATCH_SIZE * NUM_GPUS))
-    # --hf-output-len use this to increase decode ratio?
+)
+
+if (( PREFILL_ONLY > 0 )); then
+  args+=( --hf-output-len 1 )
+fi
+
+vllm bench serve "${cli_args[@]}"
--- bench_serve_8GPU.sh
+++ bench_serve_8GPU.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-./bench_serve.sh 8 8 0 32
+# prefill_only=False, chunked_prefill=False
+./bench_serve.sh 8 8 0 32 0 0
 sleep 30
 
-./bench_serve.sh 8 8 32 32
+./bench_serve.sh 8 8 32 32 0 0
 sleep 30
 
-./bench_serve.sh 8 8 64 32
+./bench_serve.sh 8 8 64 32 0 0
+sleep 30
+
+# prefill_only=True, chunked_prefill=False
+./bench_serve.sh 8 8 0 32 1 0
+sleep 30
+
+./bench_serve.sh 8 8 32 32 1 0
+sleep 30
+
+./bench_serve.sh 8 8 64 32 1 0
+sleep 30
+
+# prefill_only=False, chunked_prefill=True
+./bench_serve.sh 8 8 0 32 0 1
+sleep 30
+
+./bench_serve.sh 8 8 32 32 0 1
+sleep 30
+
+./bench_serve.sh 8 8 64 32 0 1
+sleep 30
