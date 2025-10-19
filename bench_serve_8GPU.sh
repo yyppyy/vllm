175450d36b8ce9dd09a279fc7b80f3d81da0a735
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2025-10-19 17:06:45 -0400

    finalize bench setup

--- bench_serve_8GPU.sh
+++ bench_serve_8GPU.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
 # prefill_only=False, chunked_prefill=False
-./bench_serve.sh 8 8 0 8 0 0
+./bench_serve.sh 8 8 0 16 0 0
 sleep 30
 
-./bench_serve.sh 8 8 32 8 0 0
+./bench_serve.sh 8 8 32 16 0 0
 sleep 30
 
-./bench_serve.sh 8 8 64 8 0 0
-sleep 30
-
-# prefill_only=False, chunked_prefill=True
-./bench_serve.sh 8 8 0 8 0 1
-sleep 30
-
-./bench_serve.sh 8 8 32 8 0 1
-sleep 30
-
-./bench_serve.sh 8 8 64 8 0 1
+./bench_serve.sh 8 8 64 16 0 0
