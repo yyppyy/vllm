f35d8d6be27ccf8361149d4521f11bad8023bf51
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2025-10-20 21:45:02 -0400

    switch dtype to int64

--- csrc/moe/moe_ops.h
+++ csrc/moe/moe_ops.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <torch/all.h>
-#include <cstdin>
+#include <cstdint>
 
 void topk_softmax(torch::Tensor& topk_weights, torch::Tensor& topk_indices,
                   torch::Tensor& token_expert_indices,
