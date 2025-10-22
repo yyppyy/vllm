7f174ae10de3377c2a93a085e6ffaf6e790726c2
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2025-10-21 21:42:22 -0400

    fix mem bound threshold

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -1192,7 +1192,8 @@ def eplb_map_to_physical_and_record(
         logical_replica_count: torch.Tensor,
         indices_type: Optional[torch.dtype] = None,
         mem_bound_aware_routing: Optional[str] = None,
-        router_ws: Optional[RouterWS] = None) -> torch.Tensor:
+        router_ws: Optional[RouterWS] = None,
+        mem_bound_routing_token_thres: int = 2048) -> torch.Tensor:
     '''
     Map the logical expert ids to physical expert ids
     and record the expert load metrics.
@@ -1219,7 +1220,7 @@ def eplb_map_to_physical_and_record(
     greedy_used = False
     if mem_bound_aware_routing == "greedy":
         num_pairs = topk_ids.numel()
-        if (num_pairs > 0 and router_ws.can_support(
+        if (num_pairs > 0 and topk_ids.shape[0] <= mem_bound_routing_token_thres and router_ws.can_support(
                 topk_ids, logical_to_physical_map)
                 and logical_replica_count.is_contiguous()
                 and logical_replica_count.dtype == torch.int64):
