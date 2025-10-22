ce62c84223e178fc9e70b4bcc58d65e13a91806a
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2025-10-21 21:55:58 -0400

    fix MAX_EXPERT_REDUNDANCY

--- vllm/distributed/eplb/eplb_state.py
+++ vllm/distributed/eplb/eplb_state.py
@@ -241,7 +241,7 @@ class EplbState:
         # (1023 + 1) / 8 = 128 nodes for now.
         # TODO(rui): make this configurable
         MAX_EXPERT_REDUNDANCY = 1
-        assert model.num_redundant_experts <= MAX_EXPERT_REDUNDANCY, (
+        assert model.num_redundant_experts <= MAX_EXPERT_REDUNDANCY * model.num_local_physical_experts, (
             f"num_redundant_experts {model.num_redundant_experts} "
             f"must be less than or equal to {MAX_EXPERT_REDUNDANCY}")
         max_slots_per_logical_expert = MAX_EXPERT_REDUNDANCY + 1
