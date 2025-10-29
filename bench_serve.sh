5b184f0f57de3a7845b4096879a7df5939a03948
Yanpeng Yu <yanpeng@MWMJ013E9L.yu.yale.edu>
2025-10-29 00:46:16 -0400

    fix

--- bench_serve.sh
+++ bench_serve.sh
@@ -41,9 +41,8 @@ args=(
 )
 
 # if (( NUM_REPLICAS > 0 )); then
-  USED_REPLICAS=$(( BATCH_SIZE > 16 ? 0 : NUM_REPLICAS )) # for decode only
   args+=( --enable-eplb )
-  args+=( --eplb-config "{\"window_size\":100,\"step_interval\":10000000,\"num_redundant_experts\":${USED_REPLICAS}}" )
+  args+=( --eplb-config "{\"window_size\":100,\"step_interval\":10000000,\"num_redundant_experts\":${NUM_REPLICAS}}" )
 # fi
 
 if (( BATCH_SIZE > 16 )); then
