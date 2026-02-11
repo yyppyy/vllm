e821465f50260e8927bcd8628e05ca962a73c569
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro.local>
2026-02-10 22:25:09 -0500

    temporarily unset VLLM_ALL2ALL_BACKEND

--- bench_serve.sh
+++ bench_serve.sh
@@ -58,6 +58,7 @@ fi
 unset VLLM_TORCH_PROFILER_DIR
 unset TOPK_DUMP_PREFIX
 export VLLM_ALL2ALL_BACKEND=${ALLTOALL_BACKEND}
+unset VLLM_ALL2ALL_BACKEND
 
 if (( USE_PROFILER > 0 )); then
   nsys profile \
