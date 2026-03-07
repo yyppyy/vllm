4f2a5cc60e1158c3ff93f999c439d7fb608bf4ac
Yanpeng Yu <yanpeng@Yanpengs-MacBook-Pro-2.local>
2026-03-07 00:25:40 -0500

    minor fix

--- bench_serve.sh
+++ bench_serve.sh
@@ -31,6 +31,9 @@ MAX_TOKEN_PER_BATCH=4096
 MAX_REQ_PER_BATCH=$BATCH_SIZE
 NUM_PROMPTS=$((BATCH_SIZE * NUM_GPUS))
 
+# export VLLM_DC_SPLIT_KERNELS=1 # split kernel debug
+# export VLLM_DC_SPLIT_KERNELS=1 # time breakdown debug
+
 args=(
   serve Qwen/Qwen3-30B-A3B
   --port "$PORT"
